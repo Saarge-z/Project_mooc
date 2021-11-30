e214e8aa18148c78be8073deada629068725a327
Syed Mohib Raza <razamohib01@gmail.com>
2021-11-30 02:51:36 +0000

    Data Cleaning with gender and country plot

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,19 @@
-# Example preprocessing script.
+# Load project
+#library("ProjectTemplate")
+#load.project()
+
+# Example pre-processing script.
+
+# Data set 1 
+
+enrolment = cyber.security.1_enrolments
+# viewing data 
+head(enrolment)
+
+# dropping unknown values 
+df_clean = enrolment[enrolment$gender != "Unknown" & enrolment$country != "Unknown" 
+                     & enrolment$age_range != "Unknown" & enrolment$age_range != "Unknown" & enrolment$age_range != "Unknown"
+                     & enrolment$employment_area != "Unknown",]
+
+# view cleaned data 
+head(df_clean)
--- src/eda.R
+++ src/eda.R
@@ -6,3 +6,14 @@ for (dataset in project.info$data)
   message(paste('Showing top 5 rows of', dataset))
   print(head(get(dataset)))
 }
+
+# plot(as.factor(df_clean$gender)) # calculating enrollments by gender 
+gender_plot = ggplot(df_clean, aes(x = factor(gender))) + 
+  geom_bar(stat = "count", width = 0.7, fill = "steelblue") + theme_minimal() 
+gender_plot
+
+# plot dependent of country
+country_plot = ggplot(df_clean, aes(x = factor(country))) + 
+  geom_bar(stat = "count", width = 0.7, fill = "steelblue") + theme_minimal() + 
+  scale_x_discrete(guide = guide_axis(n.dodge = 10))
+country_plot
