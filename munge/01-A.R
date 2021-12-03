75a969c0f6408b64386f042b398b770fa078b16c
Syed Mohib Raza <razamohib01@gmail.com>
2021-12-03 01:13:41 +0000

    Analysis on ques_resp data

--- munge/01-A.R
+++ munge/01-A.R
@@ -4,16 +4,25 @@
 
 # Example pre-processing script.
 
-# Data set 1 
+# Data cleaning from 1-enrollments
+enrol = cyber.security.1_enrolments
+# Displaying data and its dimensions 
+head(enrol)
+dim(enrol) # 14394 x 13
 
-enrolment = cyber.security.1_enrolments
-# viewing data 
-head(enrolment)
+# Dropping unknown values from the data set for Gender, country, age range, employment area  
+df_clean = enrol[enrol$gender != "Unknown" & enrol$country != "Unknown" 
+                     & enrol$age_range != "Unknown"
+                     & enrol$employment_area != "Unknown",]
 
-# dropping unknown values 
-df_clean = enrolment[enrolment$gender != "Unknown" & enrolment$country != "Unknown" 
-                     & enrolment$age_range != "Unknown" & enrolment$age_range != "Unknown" & enrolment$age_range != "Unknown"
-                     & enrolment$employment_area != "Unknown",]
-
-# view cleaned data 
+# Displaying cleaned data and its dimensions
 head(df_clean)
+dim(df_clean) # 1286 x 13
+
+
+# From the video stat dataset
+video_stats = as.data.frame(cyber.security.3_video.stats)
+
+
+# From question dataset
+ques_resp = cyber.security.1_question.response
--- src/eda.R
+++ src/eda.R
@@ -1,39 +1,134 @@
 library('ProjectTemplate')
 load.project()
 
-
-for (dataset in project.info$data)
-{
-  message(paste('Showing top 5 rows of', dataset))
-  print(head(get(dataset)))
-}
-
-# plot(as.factor(df_clean$gender)) # calculating enrollments by gender 
+# Calculating enrollments based on gender
+# plot(as.factor(df_clean$gender)) 
 gender_plot = ggplot(df_clean, aes(x = factor(gender))) + 
   geom_bar(stat = "count", width = 0.7, fill = "steelblue") + theme_minimal() 
 gender_plot
 
+
+# Plotting enrollments based on country
 # plot dependent of country
 country_plot = ggplot(df_clean, aes(x = factor(country))) + 
   geom_bar(stat = "count", width = 0.7, fill = "steelblue") + theme_minimal() + 
   scale_x_discrete(guide = guide_axis(n.dodge = 10))
 country_plot
 
-# plot(as.factor(df_clean$age_range)) # Identifying which age group enrolled most.
-age_range_plot = ggplot(df_clean, aes(x = factor(age_range))) + 
-  geom_bar(stat = "count", width = 0.7, fill = "steelblue") + theme_minimal() 
-age_range_plot
 
-# plot(as.factor(df_clean$employment_status)) # identifying the employment status of students enrolled 
+# Identifying most number of enrollments based on age groups
+# plot(as.factor(df_clean$age_range))
+enrol_age = ggplot(data = df_clean, aes(x = factor(age_range), 
+                          y = prop.table(stat(count)), 
+                          fill = factor(gender), 
+                          label = scales::percent(prop.table(stat(count))))) +
+  geom_bar(position = "dodge") + 
+  geom_text(stat = 'count',
+            position = position_dodge(.9), 
+            vjust = -0.5, 
+            size = 2.5) + 
+  scale_y_continuous(labels = scales::percent) + 
+  labs(x = 'Age Range', y = 'Percentage', fill = 'gear')
+enrol_age
+
+
+# Identifying no. of enrollments based on employment status of students
+# plot(as.factor(df_clean$employment_status))
 employment_plot = ggplot(df_clean, aes(x = factor(employment_status))) + 
   geom_bar(stat = "count", width = 0.7, fill = "steelblue") + theme_minimal() + 
   scale_x_discrete(guide = guide_axis(n.dodge = 2))
 employment_plot
 
 
-cc = as.factor(df_clean$employment_status) # graph is not clear 
-summary(cc)
+# Factoring based on employment status types then summarizing the data
+emp_stat = as.factor(df_clean$employment_status)
+summary(emp_stat)
 
 
+# Factoring based on highest education level types then summarizing the data
 edu_level = as.factor(df_clean$highest_education_level)
 summary(edu_level)
+
+
+#plot(as.factor(df_clean$highest_education_level))
+employment_plot = ggplot(df_clean, aes(x = factor(highest_education_level))) + 
+  geom_bar(stat = "count", width = 0.7, fill = "steelblue") + theme_minimal() + 
+  scale_x_discrete(guide = guide_axis(n.dodge = 2))
+employment_plot
+
+
+# -----------------------------------------------------------------
+
+
+
+# Displaying data & its dimensions
+dim(video_stats)
+head(video_stats)
+
+
+# Plotting box plot
+# boxplot(video_stats$north_america_views_percentage)
+box_plot = ggplot(video_stats, aes(y = north_america_views_percentage)) + 
+  geom_boxplot(fill = "steelblue")
+  #geom_point(aes(x = north_america_views_percentage))
+box_plot
+
+
+# Summarizing the data set
+summary(video_stats$north_america_views_percentage)
+
+
+# Visualizing total downloads 
+view_download = ggplot(data = video_stats, aes(y = north_america_views_percentage, x = step_position)) + 
+  geom_point(aes(colour = factor(total_downloads), size = 2))
+view_download
+
+asia_transcript = ggplot(data = video_stats, aes(x = asia_views_percentage, y = total_transcript_views)) + 
+  geom_point(aes(colour = factor(step_position), size = 2)) # scatter plot to visualize total downloads 
+asia_transcript
+
+asia_hd = ggplot(data = video_stats, aes(x = asia_views_percentage, y = viewed_hd)) + 
+  geom_point(aes(colour = factor(step_position), size = 2)) # scatter plot to visualize views in HD with respect to asia
+asia_hd
+
+high_views = max(video_stats$total_views) # calculating the highest views seen 
+high_views 
+
+low_views = min(video_stats$total_views) # calculating lowest viewership 
+low_views
+
+
+
+# -----------------------------------------------------------------------------------
+
+
+
+
+head(ques_resp)
+summary(ques_resp)
+
+
+correct_plot = ggplot(ques_resp, aes(x = factor(correct))) + 
+  geom_bar(stat = "count", width = 0.7, fill = "steelblue") + theme_minimal() 
+correct_plot
+
+
+table(ques_resp$correct)
+
+
+count_true = length(which(ques_resp$correct == "true"))
+count_true
+
+count_false = length(which(ques_resp$correct == "false"))
+count_false
+
+total_resp = length(ques_resp$correct)
+total_resp
+
+true_percent = (count_true / total_response) * 100 
+true_percent
+
+round(true_percent)
+
+false_percent = (count_false / total_response) * 100
+false_percent
